67aaeea0cb20f681d1c072845a40b64ac78a8cd1
Adithya Visnu <adithyavisnu@gmail.com>
2017-02-27 16:01:28 +0700

    add post feature get username

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser'); //get post json raw etc
 
-app.get('/', function (req,res) {
-	// body...
-	res.end("Home Page");
-});
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.post('/getin', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      username:req.body.username,
+   };
+   res.end(JSON.stringify(response));
+})
 
 var server = app.listen(8080, function () {
 	// body...
